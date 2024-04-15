6ea3f2164acda0ff2d7571ea478c68973956b83a
louisguichard <jeliraimesmailsunjour@gmail.com>
2024-04-15 14:35:22 +0200

    feat: add config

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Des chiffres et des lettres
 
 A family-friendy adaptation of the French game "Des chiffres et des lettres". Extra time for those who need it.
+
+## Usage
+
+To start the game: 
+```
+python main.py
+```
+or to launch a specific configuration:
+```
+python main.py -p family
+```
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import argparse
+import yaml
+
 from chiffres import le_compte_est_bon, solve_lceb
 from lettres import le_mot_le_plus_long, solve_lmlpl
+
+
+def load_config(profile="default"):
+    """
+    Load configuration settings from the YAML config file.
+
+    Args:
+        profile (str): Configuration profile name.
+
+    Returns:
+        dict: Dictionary containing profile-specific configurations.
+    """
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+    return config.get(profile)
+
 
 MENUS = {
     "start": {
@@ -45,11 +64,23 @@ def main():
     """
     Main function to handle the game flow based on user input.
     """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--profile",
+        type=str,
+        default="default",
+    )
+    args = parser.parse_args()
+    config = load_config(args.profile)
     action = get_user_choice("start")
     while action:
         if action == "chiffres":
             try:
-                numbers, target = le_compte_est_bon(timer=45, extra_timer=30)
+                numbers, target = le_compte_est_bon(
+                    timer=config["numbers"]["timer"],
+                    extra_timer=config["numbers"]["extra_timer"],
+                )
             except KeyboardInterrupt:
                 pass
             action = get_user_choice("numbers")
@@ -58,7 +89,10 @@ def main():
                 action = get_user_choice("numbers_sol")
         elif action == "lettres":
             try:
-                letters = le_mot_le_plus_long(timer=30, extra_timer=0)
+                letters = le_mot_le_plus_long(
+                    timer=config["letters"]["timer"],
+                    extra_timer=config["letters"]["extra_timer"],
+                )
             except KeyboardInterrupt:
                 pass
             action = get_user_choice("letters")
