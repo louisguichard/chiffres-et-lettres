e7ba94adc4e330ae48c66dbc37269272d7babff2
louisguichard <jeliraimesmailsunjour@gmail.com>
2024-05-25 20:07:12 +0200

    feat: make config customizable

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import yaml
 
 from src.chiffres import draw_numbers, solve_lceb
 from src.lettres import draw_letters, solve_lmlpl
-from src.utils import load_config
 
 app = Flask(__name__)
-config = load_config()
 
 
 @app.route("/")
@@ -13,8 +12,24 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/config", methods=["GET"])
+def get_config():
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+    return config
+
+
+@app.route("/config", methods=["POST"])
+def save_config():
+    config = request.json
+    with open("config.yaml", "w") as file:
+        yaml.safe_dump(config, file)
+    return "", 204
+
+
 @app.route("/numbers")
 def numbers_game():
+    config = get_config()
     numbers, target = draw_numbers()
     solution = solve_lceb(numbers, target)
     solution_str = "<br>".join(solution)
@@ -29,6 +44,7 @@ def numbers_game():
 
 @app.route("/letters")
 def letters_game():
+    config = get_config()
     letters = draw_letters()
     solution = solve_lmlpl(letters)
     return render_template(
@@ -37,4 +53,4 @@ def letters_game():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
